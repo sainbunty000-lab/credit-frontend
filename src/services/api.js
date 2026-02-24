b2edcf6e95969040e305aa54bba637518aecbc6a
sainbunty000-lab <sainbunty000@gmail.com>
2026-02-25 01:02:45 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,12 @@
+import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_URL;
+
+export const wcCalculate = (data) =>
+  axios.post(`${API_BASE}/wc/calculate`, data);
+
+export const agriCalculate = (data) =>
+  axios.post(`${API_BASE}/agriculture/calculate`, data);
+
+export const bankingAnalyze = (data) =>
+  axios.post(`${API_BASE}/banking/analyze`, data);
