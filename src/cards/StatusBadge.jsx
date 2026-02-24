c78429de94a39d0f082f49d98b8a1a0ba932be34
sainbunty000-lab <sainbunty000@gmail.com>
2026-02-25 01:04:30 +0530

    Update StatusBadge.jsx

--- src/cards/StatusBadge.jsx
+++ src/cards/StatusBadge.jsx
@@ -1,1 +1,10 @@
+export default function StatusBadge({ status }) {
+  const color =
+    status === "Eligible"
+      ? "text-emerald-400"
+      : status === "Rejected"
+      ? "text-red-400"
+      : "text-amber-400";
 
+  return <span className={`font-bold ${color}`}>{status}</span>;
+}
