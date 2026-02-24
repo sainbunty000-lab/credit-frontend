5d121f422405eeb303149596cb2de20c4de0bc5e
sainbunty000-lab <sainbunty000@gmail.com>
2026-02-25 01:30:09 +0530

    Update Underwriting.jsx

--- src/pages/Underwriting.jsx
+++ src/pages/Underwriting.jsx
@@ -1,1 +1,32 @@
+import WorkingCapital from "../modules/WorkingCapital/WorkingCapital";
+import Agriculture from "../modules/Agriculture/Agriculture";
+import Banking from "../modules/Banking/Banking";
 
+export default function Underwriting() {
+  return (
+    <div className="space-y-16">
+
+      {/* ===============================
+          WORKING CAPITAL MODULE
+      =============================== */}
+      <div>
+        <WorkingCapital />
+      </div>
+
+      {/* ===============================
+          AGRICULTURE MODULE
+      =============================== */}
+      <div>
+        <Agriculture />
+      </div>
+
+      {/* ===============================
+          BANKING MODULE
+      =============================== */}
+      <div>
+        <Banking />
+      </div>
+
+    </div>
+  );
+}
